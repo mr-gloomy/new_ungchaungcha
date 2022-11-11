570cafbb448806b43d9dc2850efef35a658cc655
mr-gloomy <wjddud8804@naver.com>
2022-11-11 13:29:52 +0900

    영진 11/11 13:29

--- src/main/java/com/taiso/review/action/ReviewWriteAction.java
+++ src/main/java/com/taiso/review/action/ReviewWriteAction.java
@@ -3,7 +3,6 @@ package com.taiso.review.action;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
